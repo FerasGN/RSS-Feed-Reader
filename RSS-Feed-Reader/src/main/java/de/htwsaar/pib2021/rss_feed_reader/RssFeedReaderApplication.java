68fb0a3da74daa027092d88a9814f2e6061f6699
FerasGN <firasejneid@outlook.com>
2021-04-29 16:30:10 +0200

    Set up the project. close #1

---
+++ RSS-Feed-Reader/src/main/java/de/htwsaar/pib2021/rss_feed_reader/RssFeedReaderApplication.java
@@ -0,0 +1,13 @@
+package de.htwsaar.pib2021.rss_feed_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssFeedReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssFeedReaderApplication.class, args);
+	}
+
+}
