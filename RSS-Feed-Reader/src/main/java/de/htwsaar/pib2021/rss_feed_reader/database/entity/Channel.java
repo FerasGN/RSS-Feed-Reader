8d8a878ecc8fbb5bef03eaaf585702df2b49e419
Larocheceo <rochellavofo1@gmail>
2021-05-10 17:45:38 +0200

    Entity Klassen

--- RSS-Feed-Reader/src/main/java/de/htwsaar/pib2021/rss_feed_reader/database/entity/Channel.java
+++ RSS-Feed-Reader/src/main/java/de/htwsaar/pib2021/rss_feed_reader/database/entity/Channel.java
@@ -1,5 +1,73 @@
 package de.htwsaar.pib2021.rss_feed_reader.database.entity;
 
+
+import com.sun.xml.bind.v2.TODO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity(name = "Channel")
+@Table(
+        name = "channel",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "unique_channel_name",
+                        columnNames = "name"),
+                @UniqueConstraint(name = "unique_url_channel",
+                        columnNames = "url")
+        }
+
+)
 public class Channel {
-    
+
+    @Id
+    @SequenceGenerator(
+            name = "channel_sequence",
+            sequenceName = "channel_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "channel_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+
+    private long id;
+    @Column(name = "description",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String description;
+    @Column(name = "name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String name;
+    @Column(name = "url",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String url;
+    @Column(name = "category",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String category;
+    //TODO image attribut
+
+    public Channel(long id, String description, String name, String url, String category) {
+        this.id = id;
+        this.description = description;
+        this.name = name;
+        this.url = url;
+        this.category = category;
+    }
+
+    public Channel() {
+    }
 }
