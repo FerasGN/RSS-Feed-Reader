de1993d72ca69545f5e4458ba79b798f9e536ec9
Larocheceo <rochellavofo1@gmail>
2021-05-11 11:23:22 +0200

    Added Repository classes

--- RSS-Feed-Reader/src/main/java/de/htwsaar/pib2021/rss_feed_reader/database/repository/ChannelRepository.java
+++ RSS-Feed-Reader/src/main/java/de/htwsaar/pib2021/rss_feed_reader/database/repository/ChannelRepository.java
@@ -1,5 +1,9 @@
 package de.htwsaar.pib2021.rss_feed_reader.database.repository;
 
-public interface ChannelRepository {
-    
+import de.htwsaar.pib2021.rss_feed_reader.database.entity.Channel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChannelRepository extends JpaRepository<Channel, Long> {
+
+
 }
