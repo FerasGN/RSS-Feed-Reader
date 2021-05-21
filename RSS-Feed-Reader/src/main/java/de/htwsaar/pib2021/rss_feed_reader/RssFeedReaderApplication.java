d0feae85e4a1c84e480f5c1f79a7d67a4177f8ab
FerasGN <firasejneid@outlook.com>
2021-05-21 19:11:29 +0200

    Removed unnecessary method and imports

--- RSS-Feed-Reader/src/main/java/de/htwsaar/pib2021/rss_feed_reader/RssFeedReaderApplication.java
+++ RSS-Feed-Reader/src/main/java/de/htwsaar/pib2021/rss_feed_reader/RssFeedReaderApplication.java
@@ -1,17 +1,7 @@
 package de.htwsaar.pib2021.rss_feed_reader;
 
-
-
-import de.htwsaar.pib2021.rss_feed_reader.database.entity.*;
-import de.htwsaar.pib2021.rss_feed_reader.database.repository.*;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 @SpringBootApplication
 public class RssFeedReaderApplication {
@@ -19,81 +9,6 @@ public class RssFeedReaderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RssFeedReaderApplication.class, args);
 
-	}/**
-	@Bean
-	public CommandLineRunner mappingDemo(ChannelRepository channelRepository,
-										 FeedItemRepository feedItemRepository,
-										 CategoryRepository categoryRepository,
-										 FeedItemAuthorRepository feedItemAuthorRepository,
-										 SubscriberRepository subscriberRepository,
-										 Feed_item_x_subscriberRepository feedItemXSubscriberRepository,
-										 Channel_subscriberRepository channelSubscriberRepository){
-		return args -> {
-			Set<Feed_item_x_subscriber> feed_item_x_subscriberSet = new HashSet<>();
-			Set<Channel_subscriber> channel_subscriberSet = new HashSet<>();
-
-
-			//create subscriber
-			Subscriber subscriber = new Subscriber("rochella", "vofo", "rochelvofo@yahoo.com", "1254",
-					"germany", "student", 21, channel_subscriberSet, feed_item_x_subscriberSet);
-			subscriberRepository.save(subscriber);
-
-			//create channel category
-			Category category = new Category("politik");
-			categoryRepository.save(category);
-
-			//create channel
-			Channel channel = new Channel("Channel description", " channel name",
-					"facebook.com", category);
-			channelRepository.save(channel);
-			Channel channel2 = new Channel("Channel description", " channel name2",
-					"vinoleed.com", category);
-			channelRepository.save(channel2);
-
-
-			//create feeditem
-			FeedItem feedItem = new FeedItem( "feed content", "feed description",
-					"feed link", "feed title", new Date(12385), channel,
-					feed_item_x_subscriberSet, category);
-			feedItemRepository.save(feedItem);
-
-
-			//create feedItemAuthor
-			FeedItemAuthor feedItemAuthor = new FeedItemAuthor("Garri Zingraff"); //Das object Feeditem wird jetzt übergeben
-			feedItemAuthorRepository.save(feedItemAuthor);
-
-			//create channel subscriber
-			Channel_subscriber channel_subscriber = new Channel_subscriber(
-					true, subscriber, channel
-			);
-			Channel_subscriber channel_subscriber2 = new Channel_subscriber(
-					true, subscriber, channel2
-			);
-			channel_subscriberSet.add(channel_subscriber);
-			channel_subscriberSet.add(channel_subscriber2);
-			channelSubscriberRepository.save(channel_subscriber);
-
-			//create Feed_item_x_subscriber
-			Feed_item_x_subscriber feed_item_x_subscriber = new Feed_item_x_subscriber(
-					4, true, 2, true, feedItem, subscriber);
-			feed_item_x_subscriberSet.add(feed_item_x_subscriber);
-			feedItemXSubscriberRepository.save(feed_item_x_subscriber);
-
-
-
-
-
-
-			//save all in repositories
-
-
-
-
-
-
-
-
-		};
-	}*/
+	}
 
 }
